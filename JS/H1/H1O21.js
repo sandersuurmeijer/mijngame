6eca8ff512f0ba7e409af741a24b7728aaa31641
sandersuurmeijer <152558@csgaugustinus.nl>
2020-12-01 09:05:35 +0000

    les

--- JS/H1/H1O21.js
+++ JS/H1/H1O21.js
@@ -20,17 +20,24 @@ function draw() {
   stroke('white');
   strokeWeight(10);
 
-  if (mouseIsPressed == true && snelheid == 5) {
-      fill('green');
+
+    if (snelheid == 5){
+      fill("green")
   }
+
   else {
     fill('dodgerblue');
   }
-  
-  if (x>880) {
-    snelheid = -1*snelheid;
+
+   if (mouseIsPressed == false) {
+       diameter = 100
+   }
+  else {
+      diameter = 200
+      fill ('red')
   }
-  if (x<120) {
+
+  if (x>880 || x<120) {
     snelheid = -1*snelheid;
   }
 
