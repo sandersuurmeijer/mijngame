ecf6171e4599319ddacd3455b0c75ecfc8ea32fd
sandersuurmeijer <152558@csgaugustinus.nl>
2020-12-02 14:18:40 +0000

    LES

--- JS/H1/H1O25.js
+++ JS/H1/H1O25.js
@@ -28,22 +28,23 @@ function draw() {
   fill(255,255,255,1);
   ellipse(xPositie,yPositie,2*straal);  
   
-  // Pas onderstaande regel aan: gebruik de functie dist om de onderlinge afstand te bepalen
   
-  onderlingeAfstand = 1;
-  if (onderlingeAfstand <= 0) {
-    eindScherm();
-    noLoop();
-  }
+ onderlingeAfstand=round(dist(550,height - straal,xPositie,yPositie)) - 2*straal;
+ // if (onderlingeAfstand <= 0) {
+    //eindScherm();
+   // noLoop();
+//  }
   
-  // door de slashes weg te halen kun je besturing van de bal inschakelen.
   // Om het wat moeilijker te maken veranderen er bij gebruik van een pijltoets 2 dingen tegelijkertijd.
   
-  // gebruikBesturing();
+  gebruikBesturing();
 
-  if (yPositie<straal || yPositie>height-straal) {
+    if (yPositie<straal || yPositie>height - straal) {
     ySnelheid *= -1;
-  } 
+  }
+  if (xPositie<straal || xPositie>width - straal) {
+    xSnelheid *= -1;
+  }  
 }
 
 function gebruikBesturing() {
