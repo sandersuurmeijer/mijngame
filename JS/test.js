3654214136ba7948b4de8fdabd2730129803ed9f
sandersuurmeijer <152558@csgaugustinus.nl>
2021-08-31 08:07:53 +0000

    las

--- JS/test.js
+++ JS/test.js
@@ -3,12 +3,29 @@ function setup() {
   background('silver');
   canvas.parent('processing');
   //noLoop();
+
 }
 
 function draw() {
   noStroke();
-  fill('steelblue');
-  ellipse(0,0,800);
-  fill('deepskyblue');
-  ellipse(450,450,400);
+
+for(var n=0; n<5; n++) {
+  fill('red')
+  if ( n=3){
+    fill('blue')
+  }
+  rect(0,250,50,50)
+  translate(100,0)
+  
+}
+
+
+
+
+
+
+// fill('steelblue');
+//  ellipse(0,0,800);
+//  fill('deepskyblue');
+//  ellipse(450,450,400);
 }
